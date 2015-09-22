300f471682471097aa622c1425eba2fd44101047
Krosen11 <larrybarnson@gmail.com>
2015-09-21 23:58:36 -0500

    Map is blank right now. If remove the Places stuff, seems to work

--- map.js
+++ map.js
@@ -1,7 +1,12 @@
+$("#food").click(getPlaces);
+$("#bars").click(getPlaces);
+
+var testing = null;
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 6
+        center: {lat: 30.2500, lng: 97.7500},
+        zoom: 12
     });
     var infoWindow = new google.maps.InfoWindow({map: map});
 
@@ -12,6 +17,8 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         };
+            
+        testing = pos;
 
         infoWindow.setPosition(pos);
         infoWindow.setContent('Location found.');
@@ -31,4 +38,38 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
+}
+
+function getPlaces(event) {
+    if ($(this).attr("id") === "food") {
+        var service = new google.maps.places.PlacesService(map);
+          service.nearbySearch({
+            location: testing,
+            radius: 2000, //2km search radius
+            types: ['food']
+          }, callback);
+    }
+    else if ($(this).attr("id") === "bars") {
+    }
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    setMap: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
